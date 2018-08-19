c5c51c5dd447163ebbd4ea3fa754518382ef9fca
scarlettperry <si.perry15@gmail.com>
2018-08-19 14:24:11 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,5 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true
-  validates :title, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :artist_name, presence: true
   validates :released, inclusion: {in: [true, false]} #dont need presence for boolean
   validate :year?
@@ -12,5 +11,5 @@ class Song < ActiveRecord::Base
       errors.add(:release_year, "Date must be less than or equal to the current year")
     end
   end
-  
+
 end
